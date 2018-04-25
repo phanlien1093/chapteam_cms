18d9132302c0bad00bc6943ecd4bd60a26788c97
Phan Liền <phanlien1093@gmail.com>
2018-04-25 14:37:11 +0700

    Lien Push

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group([
+	'prefix' => 'admin'
+], function(){
+	Route::get('/', 'HomeController@index');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
